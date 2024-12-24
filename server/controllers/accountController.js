38fccf7d93c45505fc49bcbb4bf511faaafd494f
Sastraaaa <kharismasastrawansyah@gmail.com>
2024-12-24 09:08:32 +0700

    feat: Implement user authentication
    Add user registration and login functionality.
    Use JWT for secure authentication and authorization.
    Improve application security with password hashing.

--- server/controllers/accountController.js
+++ server/controllers/accountController.js
@@ -1,7 +1,9 @@
 const Account = require("../models/accountModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const accountController = {
+  // Method get all account
   async getAll(req, res) {
     try {
       const accounts = await Account.getAll();
@@ -11,6 +13,7 @@ const accountController = {
     }
   },
 
+  // Method get account by id
   async getById(req, res) {
     try {
       const account = await Account.getById(req.params.id);
@@ -21,6 +24,7 @@ const accountController = {
     }
   },
 
+  // method create
   async create(req, res) {
     try {
       const { name, email, password, role } = req.body;
@@ -37,6 +41,7 @@ const accountController = {
     }
   },
 
+  // method update
   async update(req, res) {
     try {
       const { name, email, role } = req.body;
@@ -47,10 +52,41 @@ const accountController = {
     }
   },
 
+  // method delete
   async delete(req, res) {
     try {
       await Account.delete(req.params.id);
       res.status(200).json({ message: "Account deleted successfully" });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  },
+
+  // method login
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
+
+      // Periksa apakah email ada di database
+      const account = await Account.getByEmail(email);
+      if (!account) return res.status(404).json({ error: "Account not found" });
+
+      // Periksa password
+      const isPasswordValid = await bcrypt.compare(
+        password,
+        account.account_password
+      );
+      if (!isPasswordValid)
+        return res.status(401).json({ error: "Invalid password" });
+
+      // Generate JWT
+      const token = jwt.sign(
+        { id: account.account_id, role: account.role },
+        process.env.JWT_SECRET,
+        { expiresIn: "1h" }
+      );
+
+      res.status(200).json({ token });
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
