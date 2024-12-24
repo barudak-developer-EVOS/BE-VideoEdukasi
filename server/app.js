0d33efad6f9b6eb5ef51639ebbd5900d8894bf16
Sastraaaa <kharismasastrawansyah@gmail.com>
2024-12-24 16:16:38 +0700

    feat: Implement video comment functionality
    Add a new endpoint for managing video comments.
    
    Enable users to post, read, update, and delete comments.
    
    Improve user engagement and interaction with videos.

--- server/app.js
+++ server/app.js
@@ -3,12 +3,19 @@ const bodyParser = require("body-parser");
 const accountRoutes = require("./routes/accountRoutes");
 const videoRoutes = require("./routes/videoRoutes");
 const path = require("path");
-
 const app = express();
+const commentRoutes = require("./routes/commentRoutes");
+
+// Parsing JSON
+app.use(express.json());
+
+// Parsing URL-encoded form data
+app.use(express.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 app.use("/api/accounts", accountRoutes);
 app.use("/api/videos", videoRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
+app.use("/api/comments", commentRoutes);
 
 module.exports = app;
