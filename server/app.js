c1e01c6012fa2e754484e376bb943a458d066f5f
Sastraaaa <kharismasastrawansyah@gmail.com>
2024-12-24 15:37:29 +0700

    feat: Implement video upload functionality
    Add new endpoints for video upload and retrieval.
    Enable users to upload and manage their video content.
    Include necessary authentication and authorization checks.
    Improve overall user experience.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const accountRoutes = require("./routes/accountRoutes");
+const videoRoutes = require("./routes/videoRoutes");
+const path = require("path");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use("/api/accounts", accountRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
 
 module.exports = app;
