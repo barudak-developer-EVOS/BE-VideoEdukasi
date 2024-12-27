4b2facf2c76e0883952b4dd5a122e62d87069000
Sastraaaa <kharismasastrawansyah@gmail.com>
2024-12-28 06:23:40 +0700

    feat: Enhance account management routes
    Add profile photo upload functionality to account creation.
    
    Restrict account update and deletion to authorized roles.
    Improve route security and functionality.

--- server/routes/accountRoutes.js
+++ server/routes/accountRoutes.js
@@ -3,18 +3,19 @@ const accountController = require("../controllers/accountController");
 const router = express.Router();
 const {
   authMiddleware,
-  // roleMiddleware,
+  roleMiddleware,
 } = require("../middleware/authMiddleware");
+const upload = require("../middleware/uploadMiddleware");
 const { validateLogin } = require("../middleware/validationMiddleware");
 
 // Public routes
 router.post("/login", validateLogin, accountController.login);
-router.post("/", accountController.create);
+router.post("/", upload.single("profilePhoto"), accountController.create);
 
 // Protected routes
 router.get("/", authMiddleware, accountController.getAll);
 router.get("/:id", authMiddleware, accountController.getById);
-router.put("/:id", authMiddleware, accountController.update);
-router.delete("/:id", authMiddleware, accountController.delete);
+router.put("/:id", roleMiddleware, authMiddleware, accountController.update);
+router.delete("/:id", roleMiddleware, authMiddleware, accountController.delete);
 
 module.exports = router;
