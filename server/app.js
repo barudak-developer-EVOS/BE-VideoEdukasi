162439c9b59b7ef4a1ac20821cdc934b60b06b89
Sastraaaa <kharismasastrawansyah@gmail.com>
2024-12-28 19:48:52 +0700

    feat: implement swagger documentation
    - Added swagger-ui-express for API docs
    - Created swagger configuration file
    - Integrated docs into app routes

--- server/app.js
+++ server/app.js
@@ -5,17 +5,18 @@ const videoRoutes = require("./routes/videoRoutes");
 const commentRoutes = require("./routes/commentRoutes");
 const path = require("path");
 const app = express();
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./config/swaggerConfig");
 
-// Parsing JSON
-app.use(express.json());
+app.use(express.json()); // Parsing JSON
+app.use(express.urlencoded({ extended: true })); // Parsing URL-encoded form data
+app.use(bodyParser.json()); // Parsing JSON
+app.use("/api/accounts", accountRoutes); // Middleware for account routes
+app.use("/api/videos", videoRoutes); // Middleware for video routes
+app.use("/api/comments", commentRoutes); // Middleware for comment routes
+app.use("/uploads", express.static(path.join(__dirname, "./uploads"))); // Middleware for serving static files
 
-// Parsing URL-encoded form data
-app.use(express.urlencoded({ extended: true }));
-
-app.use(bodyParser.json());
-app.use("/api/accounts", accountRoutes);
-app.use("/api/videos", videoRoutes);
-app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
-app.use("/api/comments", commentRoutes);
+// swagger documentation
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 module.exports = app;
